3c383f3415b2eecf008487a973b2ea64fef87029
AmosJohanPersson <114666197+AmosJohanPersson@users.noreply.github.com>
2022-11-21 16:51:49 +0100

    click to pick up implemented

--- Assets/Scripts/GrabBehaviour.cs
+++ Assets/Scripts/GrabBehaviour.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +6,27 @@ public class GrabBehaviour : MonoBehaviour
     [SerializeField] GameObject handPoint;
 
     private Camera cam;
+    private bool isCarrying;
 
     private void Start()
     {
         cam = Camera.main;
+        isCarrying = false;
     }
 
     private void OnFire(InputValue input)
     {
+        Vector3 lookingPoint = new Vector3(0.5f, 0.5f, 0f);
+        Ray ray = cam.ViewportPointToRay(lookingPoint);
+        RaycastHit hit;
+
+        bool didHit = Physics.Raycast(ray, out hit);
+        GameObject other = didHit ? hit.collider.gameObject : null;
+
+        if (didHit && other.CompareTag("Grabbable") && !isCarrying)
+        {
+            other.GetComponent<ObjectBehaviour>().PickUp(handPoint.transform.position);
+            isCarrying = true;
+        }
     }
 }
--- Assets/Scripts/ObjectBehaviour.cs
+++ Assets/Scripts/ObjectBehaviour.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+enum ObjectInteractionState
+{
+    held,
+    up,
+    down,
+    put
+}
+
 public class ObjectBehaviour : MonoBehaviour
 {
 
@@ -12,34 +20,73 @@ public class ObjectBehaviour : MonoBehaviour
     [SerializeField] private float launchDuration;
 
     private Vector3 origin;
+    private Vector3 moveTarget;
     private Rigidbody rigid;
     private float timeSinceInteract;
+    private ObjectInteractionState state;
 
     private void Start()
     {
         origin = transform.position;
         rigid = GetComponent<Rigidbody>();
         timeSinceInteract = 0;
+        state = ObjectInteractionState.put;
+        moveTarget = origin;
     }
 
     private void Update()
     {
-        HandleMovement(cam.transform.position + new Vector3(-.25f, -.5f, .15f));
+        HandleMovement(moveTarget);
     }
 
     private void HandleMovement(Vector3 target)
     {
         timeSinceInteract += Time.deltaTime;
-        float interpolation = (launchDuration - timeSinceInteract) / launchDuration;
-        Vector3 direction = (target - transform.position) + launch * upCurve.Evaluate(interpolation) * Vector3.up;
-        Vector3 translation = speed * Time.deltaTime * direction.normalized;
-        if (Vector3.Magnitude(translation) < Vector3.Distance(transform.position, target))
+
+        switch (state)
         {
-            rigid.MovePosition(transform.position + translation);
+            case ObjectInteractionState.up:
+                float interpolation = (launchDuration - timeSinceInteract) / launchDuration;
+                Vector3 direction = (target - transform.position) + launch * upCurve.Evaluate(interpolation) * Vector3.up;
+                Vector3 translation = speed * Time.deltaTime * direction.normalized;
+
+                if (Vector3.Magnitude(translation) < Vector3.Distance(transform.position, target))
+                {
+                    rigid.MovePosition(transform.position + translation);
+                }
+                else
+                {
+                    rigid.MovePosition(target);
+                    state = ObjectInteractionState.held;
+                }
+                break;
+            case ObjectInteractionState.held:
+                //move with player
+                break;
+            case ObjectInteractionState.down:
+                break;
+            case ObjectInteractionState.put:
+                break;
         }
-        else
+    }
+
+    public void PickUp(Vector3 targetPosition)
+    {
+        if (state == ObjectInteractionState.put)
         {
-            rigid.MovePosition(target);
+            timeSinceInteract = 0;
+            state = ObjectInteractionState.up;
+            moveTarget = targetPosition;
+        }
+    }
+
+    public void PutDown(Vector3 targetPosition)
+    {
+        if (state == ObjectInteractionState.held)
+        {
+            timeSinceInteract = 0;
+            state = ObjectInteractionState.down;
+            moveTarget = targetPosition;
         }
     }
 }
