698e2fc75b7ef4a8b377e9e56fa3c448bf5e841b
AmosJohanPersson <114666197+AmosJohanPersson@users.noreply.github.com>
2022-11-23 14:33:38 +0100

    fonts and ui implemented in lab

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_Text progress;
+    [SerializeField] private TMP_Text goToBed;
+}
